5d02d752f153dfa4362b869e74860a316b309418
0LongDuy0 <longduywork@gmail.com>
2023-01-28 15:59:05 +0700

    C3: Commit cai dat co ban

---
+++ Staging_Project/Staging_Utility/U_Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staging_Utility
+{
+    public class U_Config
+    {
+        public static string Project_Staging_Conn_String = "";
+
+        public static string Date_Format_String = "dd/MM/yyyy";
+
+        public static string Number_Format_String = "###,###0.###;-###,###0.###;-";
+    }
+}
---
+++ Staging_Project/Staging_Utility/U_Utility.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staging_Utility
+{
+    public class U_Utility
+    {
+        public static Random rand = new Random();
+
+        #region Default
+        public static DateTime? Get_Null_Date()
+        {
+            return Convert_String_To_Datetime("01/01/1900", "dd/MM/yyyy");
+        }
+
+        public static int Convert_To_Int32(object p_objData)
+        {
+            if ((p_objData != System.DBNull.Value) && (U_Utility.Convert_To_String(p_objData) != ""))
+                return Convert.ToInt32(p_objData);
+            else
+                return U_Const.INT_VALUE_NULL;
+        }
+
+        public static long Convert_To_Int64(object p_objData)
+        {
+            if ((p_objData != System.DBNull.Value) && (U_Utility.Convert_To_String(p_objData) != ""))
+                return Convert.ToInt64(p_objData);
+            else
+                return U_Const.INT_VALUE_NULL;
+        }
+
+        public static double Convert_To_Double(object p_objData)
+        {
+            if ((p_objData != System.DBNull.Value) && (U_Utility.Convert_To_String(p_objData) != ""))
+                return Convert.ToDouble(p_objData);
+            else
+                return U_Const.FLT_VALUE_NULL;
+        }
+
+        public static bool Convert_To_Bool(object p_objData)
+        {
+            if (p_objData != System.DBNull.Value)
+                return Convert.ToBoolean(p_objData);
+            else
+                return U_Const.BL_VALUE_NULL;
+        }
+
+        public static DateTime? Convert_To_DateTime(object p_objData)
+        {
+            if (p_objData != System.DBNull.Value && p_objData != null)
+                return Convert.ToDateTime(p_objData);
+            else
+                return U_Const.DTM_VALUE_NULL;
+        }
+
+        public static DateTime? Convert_String_To_Datetime(string p_strDate)
+        {
+            if (p_strDate == "")
+                return U_Const.DTM_VALUE_NULL;
+
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            return DateTime.ParseExact(p_strDate, U_Config.Date_Format_String, provider);
+        }
+
+        public static DateTime? Convert_String_To_Datetime(string p_strDate, string p_strFormat)
+        {
+            if (p_strDate == "")
+                return U_Const.DTM_VALUE_NULL;
+
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            return DateTime.ParseExact(p_strDate, p_strFormat, provider);
+        }
+
+        public static string Convert_To_String(object p_objData)
+        {
+            return Convert.ToString(p_objData);
+        }
+        #endregion
+    }
+}
